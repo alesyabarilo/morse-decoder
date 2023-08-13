a93a276ac36ca545c8a0468449478c711d725eab
Alesya Barilo <bariloalesya@yandex.ru>
2023-08-13 16:58:05 +0300

    feat: solve morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const array = [];
+    
+    for (let i = 0; i < expr.length; i++) {
+        if (i % 10 === 0) {
+        array.push(expr.slice(i, i+10))
+        }
+    }
+
+    const cutArray = array.map( item => {
+        for (let i = 0; i < item.length; i++) {
+            if (item[i] === '1') return item.slice(i);
+
+            if (item[0] === '*') return item;
+            }
+        });
+
+    const morseArray = cutArray.map( item => {
+        if (item[0] === '*') return item;
+        let signs = '';
+        for (let i = 0; i < item.length; i = i + 2) {
+            if (item[i + 1] === '0') {
+                signs += '.';
+            } else {
+                signs += '-'; 
+            }
+        }
+        return signs;
+    })
+
+    return morseArray.map( item => {
+        if (item[0] === '*') return ' ';
+        for (let key in MORSE_TABLE) {
+            if (key === item) return MORSE_TABLE[key];
+        }
+    }).join('');
 }
 
 module.exports = {
